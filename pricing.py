54281ced9fcc23955db4692c25e8f138b2a06fac
Muhammad Ahmad Farooq <ahmad.farooq1206@gmail.com>
2026-09-26 18:28:15 +0100

    Update pricing.py

--- pricing.py
+++ pricing.py
@@ -1,18 +1,18 @@
-#performance of code may vary with time of day
+#Performance of code may vary with time of day
 
 import pandas as pd
 import numpy as np
 import yfinance as yf
 from bsm import Call,Put
 
-#acquires asset
+#Acquires asset
 ticker=input('Please enter the ticker for the stock or ETF on Yahoo Finance you would like an option for? ')
-#loads data for asset
+#Loads data for asset
 data=yf.download(ticker,period='1y',multi_level_index=False)
-#retrieves current value of asset
+#Retrieves current value of asset
 current=round(float(data['Close'].tail(1).to_numpy()[0]),2) 
 
-#checks for dividends if available
+#Checks for dividends if available
 tick=yf.Ticker(ticker)
 try:
     dividend_yield=tick.info['dividendYield']/100
@@ -20,22 +20,22 @@ except KeyError:
     dividend_yield=0
 
 df=pd.DataFrame(data)
-#calculates logarithmic returns over the past year
+#Calculates logarithmic returns over the past year
 df['log_returns']=np.log(df['Close']/df['Close'].shift(1))
-#calculates annualised historical volatility
+#Calculates annualised historical volatility
 volatility=df['log_returns'].std()*252**.5
 
-#asks for strike price
+#Asks for strike price
 strike=float(input(f'The current price of {ticker} is {current}.\nWhat strike price would you like? '))
-#asks for time to maturity
+#Asks for time to maturity
 time=float(input(f'How long, as a decimal in years, would you like to hold your {ticker} option for? '))
-#asks for local bond yields rate
+#Asks for local bond yields rate
 risk_free_interest_rate=float(input(f'Enter the risk free interest rate of the currency {ticker} is denominated in as a decimal '))
 
 info=[current,strike,time,risk_free_interest_rate,dividend_yield,volatility]
 
 call,put=Call(*info).round(2),Put(*info).round(2)
 
-#gives option prices
+#Gives option prices
 print(f'Call Price: {call}')
 print(f'Put Price: {put}')
