183a8a85d6ce55d066d0fd16492fe674b387cf20
Muhammad Ahmad Farooq <ahmad.farooq1206@gmail.com>
2026-09-26 18:27:04 +0100

    Update bsm.py

--- bsm.py
+++ bsm.py
@@ -1,14 +1,17 @@
 import numpy as np
 from scipy.special import erf
 
+#Standard Normal cumulative distribution function
 def N(z):
     return .5*(1+erf(z/2**-.5))
 
+#Call price
 def Call(current,strike,time,risk_free_interest_rate,dividend_yield,volatility):
     d1=(np.log(current/strike)+time*(risk_free_interest_rate-dividend_yield+0.5*volatility**2))/(volatility*time**0.5)
     d2=d1-volatility*time**0.5
     return current*np.exp(-dividend_yield*time)*N(d1)-strike*np.exp(-risk_free_interest_rate*time)*N(d2)
 
+#Put price
 def Put(current,strike,time,risk_free_interest_rate,dividend_yield,volatility):
     d1=(np.log(current/strike)+time*(risk_free_interest_rate-dividend_yield+0.5*volatility**2))/(volatility*time**0.5)
     d2=d1-volatility*time**0.5
