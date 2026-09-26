5fbe160480add6928831af0ac94a4519308b936b
Muhammad Ahmad Farooq <ahmad.farooq1206@gmail.com>
2026-09-26 18:43:33 +0100

    Update bsm.py

--- bsm.py
+++ bsm.py
@@ -7,12 +7,12 @@ def N(z):
 
 #Call price formula
 def Call(current,strike,time,risk_free_interest_rate,dividend_yield,volatility):
-    d1=(np.log(current/strike)+time*(risk_free_interest_rate-dividend_yield+0.5*volatility**2))/(volatility*time**0.5)
-    d2=d1-volatility*time**0.5
+    d1=(np.log(current/strike)+time*(risk_free_interest_rate-dividend_yield+.5*volatility**2))/(volatility*time**.5)
+    d2=d1-volatility*time**.5
     return current*np.exp(-dividend_yield*time)*N(d1)-strike*np.exp(-risk_free_interest_rate*time)*N(d2)
 
 #Put price formula
 def Put(current,strike,time,risk_free_interest_rate,dividend_yield,volatility):
-    d1=(np.log(current/strike)+time*(risk_free_interest_rate-dividend_yield+0.5*volatility**2))/(volatility*time**0.5)
-    d2=d1-volatility*time**0.5
+    d1=(np.log(current/strike)+time*(risk_free_interest_rate-dividend_yield+.5*volatility**2))/(volatility*time**.5)
+    d2=d1-volatility*time**.5
     return strike*np.exp(-risk_free_interest_rate*time)*N(-d2)-current*np.exp(-dividend_yield*time)*N(-d1)
