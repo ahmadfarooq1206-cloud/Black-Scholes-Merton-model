b2152e80caa9cec0311bcd1f8eaafca20ed6b88e
Muhammad Ahmad Farooq <ahmad.farooq1206@gmail.com>
2026-09-26 18:42:49 +0100

    Update bsm.py

--- bsm.py
+++ bsm.py
@@ -5,13 +5,13 @@ from scipy.special import erf
 def N(z):
     return .5*(1+erf(z/2**-.5))
 
-#Call price
+#Call price formula
 def Call(current,strike,time,risk_free_interest_rate,dividend_yield,volatility):
     d1=(np.log(current/strike)+time*(risk_free_interest_rate-dividend_yield+0.5*volatility**2))/(volatility*time**0.5)
     d2=d1-volatility*time**0.5
     return current*np.exp(-dividend_yield*time)*N(d1)-strike*np.exp(-risk_free_interest_rate*time)*N(d2)
 
-#Put price
+#Put price formula
 def Put(current,strike,time,risk_free_interest_rate,dividend_yield,volatility):
     d1=(np.log(current/strike)+time*(risk_free_interest_rate-dividend_yield+0.5*volatility**2))/(volatility*time**0.5)
     d2=d1-volatility*time**0.5
